413cbcc852010e688f27f42b32d09ec8ebf0d2f1
michaelwave537 <xmichael537@gmail.com>
2026-06-05 20:16:04 -0700

    <new: solution for problem 2574. Left and Right Sum Differences>

---
+++ solutions/easy/2574 Left and Right Sum Differences.py
@@ -0,0 +1,14 @@
+class Solution:
+    def leftRightDifference(self, nums: List[int]) -> List[int]:
+        leftSum = [0] * len(nums)
+        rightSum = [0] * len(nums)
+        sol = []
+        for i in range(1, len(nums)):
+            leftSum[i] = leftSum[i-1] + nums[i-1]
+            rightSum[len(nums)-i-1] = rightSum[len(nums)-i] + nums[len(nums)-i]
+            
+        
+        for i in range(len(nums)):
+            sol.append(abs(leftSum[i]-rightSum[i]))
+
+        return sol
